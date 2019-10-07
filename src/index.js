47d38d0b5ba03da6485bb23f6918776fd8738d00
Alelizsanchez <Alelizsanchez@gmail.com>
2019-10-07 15:01:33 -0500

    actualizando cambios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-import validator from './validator.js';
+import { isValid } from './validator.js';
 
-console.log(validator);
+let template = `<div class="cars__title">LEGOLAND</div>
+<p>validar pagos con tarjetas virtuales para juegos en online</p>
+<img src="./img/dota2.jpg" width="200" heigth="150" id="img-uno" >
+<img src="./img/descarga.jpg" width="200" heigth="150" id="img-dos">
+<img src="./img/league.jpg" width="200" heigth="150" id="img-tres" >
+<img src="./img/pokemongo.jpg" width="200" heigth="150" id="img-cuatro" >
+<div class="cars">
+      <form class="cars__form">
+      
+          <label class="cars__label">Usuario:</label>
+          <input class="cars__input" id="btn-input" type="text" placeholder="Ingresa tu numero de tarjeta"/>
+          <button type="button" class="cars__button" id="btn-validar">Validar</button>
+        </div>
+      </form>
+    </div>`;
+
+document.getElementById('contenedor').innerHTML = template;
+const btnvalidar = document.getElementById('btn-validar');
+
+btnvalidar.addEventListener('click', (event) => {
+  event.preventDefault;
+  let valorInput = document.getElementById('btn-input');
+  isValid(valorInput);
+  const pintar = isValid(valorInput);
+  document.getElementById('resultado').innerHTML = pintar
+  // console.log(isValid(valorInput));
+});
+
+document.getElementById('img-uno').addEventListener('click', () => {
+document.write('dota');
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,30 @@
-const validator = {
-  // ...
-};
+export const isValid = (creditCardNumber) => {
 
-export default validator;
+  let sumaImpar = 0;
+  let sumaPar = 0;
+  const numToString = creditCardNumber.value.toString().split("");
+
+  for (let i = 0; i < numToString.length; i++) {
+    if (i % 2 === 0) {
+      if (numToString[i] * 2 >= 10) {
+        sumaPar += ((numToString[i] * 2) - 9);
+      } else {
+        sumaPar += numToString[i] * 2;
+      }
+    }
+
+    else {
+      sumaImpar += parseInt(numToString[i]);
+    }
+  }
+  if ((sumaImpar + sumaPar) % 10 === 0 === true) {
+    return 'valido';
+  } else {
+    return 'invalido';
+  }
+}
+
+
+
+
+
